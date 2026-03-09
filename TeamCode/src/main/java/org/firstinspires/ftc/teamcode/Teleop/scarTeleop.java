c987c7ffe1bc0baa0313b192fa25bc30dd1495e7
Shanaya <shanaya@MacBook-Air-352.local>
2026-03-09 16:43:04 -0400

    dependencies

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/scarTeleop.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.Teleop;
+
+public class scarTeleop {
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsytem/drivesubsystemscar.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.subsytem;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+public class drivesubsystemscar {
+}
