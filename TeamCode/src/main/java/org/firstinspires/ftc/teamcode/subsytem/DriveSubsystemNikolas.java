e618398b6feb7baf81a7bab6d18e17f60743f135
Nikolas McMahon <nikmc7877@gmail.com>
2026-03-11 15:58:17 -0400

    this might be done

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsytem/DriveSubsystemNikolas.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.subsytem;
+
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+
+public class DriveSubsystemNikolas {
+    public final Motor frontLeft;
+    public final Motor frontRight;
+    public final Motor backLeft;
+    public final Motor backRight;
+
+    public DriveSubsystemNikolas(Motor fL, Motor fR, Motor bL, Motor bR) {
+        frontLeft = fL;
+        frontRight = fR;
+        backLeft = bL;
+        backRight = bR;
+    }
+
+    public void drivePowers(double forward, double strafe, double turn) {
+        double flP = clamp(forward, strafe, turn);
+        double frP = clamp(forward, -strafe, -turn);
+        double blP = clamp(forward, -strafe, turn);
+        double brP = clamp(forward, strafe, -turn);
+
+        //double max = Math.abs(forward) + Math.abs(strafe);
+        
+    }
+    public double clamp(double f, double s, double t) {
+        if (f + s + t > 1) {
+            return 1;
+        } else if (f + s + t < -1) {
+            return -1;
+        } else {
+            return f + s + t;
+        }
+    }
+}
