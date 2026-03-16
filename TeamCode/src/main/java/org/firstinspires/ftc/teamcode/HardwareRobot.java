3bdae73a34ae03b30acfd236e95d7ccede62325f
grek2 <madisonwharton0@gmail.com>
2026-03-16 15:33:27 -0400

    spaghetti hardwarerobot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareRobot.java
@@ -0,0 +1,77 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+public class HardwareRobot {
+
+    public final Motor leftFront;
+    public final Motor rightFront;
+    public final Motor leftBack;
+    public final Motor rightBack;
+    public final WebcamName cameraName;
+    public final IMU imu;
+
+    public HardwareRobot(HardwareMap hardwareMap) {
+        leftFront = new Motor(hardwareMap, "leftFront", Motor.GoBILDA.RPM_312);
+        rightFront = new Motor(hardwareMap, "rightFront", Motor.GoBILDA.RPM_312);
+        leftBack = new Motor(hardwareMap, "leftBack", Motor.GoBILDA.RPM_312);
+        rightBack = new Motor(hardwareMap, "rightBack", Motor.GoBILDA.RPM_312);
+
+        leftFront.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftFront.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFront.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBack.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBack.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        leftFront.setRunMode(Motor.RunMode.RawPower);
+        rightFront.setRunMode(Motor.RunMode.RawPower);
+        leftBack.setRunMode(Motor.RunMode.RawPower);
+        rightBack.setRunMode(Motor.RunMode.RawPower);
+
+        leftFront.setInverted(true);
+        rightFront.setInverted(true);
+        leftBack.setInverted(true);
+        rightBack.setInverted(true);
+
+        leftFront.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftFront.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        imu = hardwareMap.get(IMU.class, "imu");
+        cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+    }
+    public void setImu() {
+        RevHubOrientationOnRobot orientationOnRobot =
+                new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                        RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+                );
+
+        // Create parameters with orientation + angle unit
+        IMU.Parameters parameters = new IMU.Parameters(orientationOnRobot);
+
+        imu.initialize(parameters);
+        imu.resetYaw();
+    }
+    public double getHeading() {
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        return orientation.getYaw(AngleUnit.DEGREES);
+    }
+}
