92d4577ff80f5a389541113880b5a7ffb480613d
Nikolas McMahon <nikmc7877@gmail.com>
2026-04-06 15:57:08 -0400

    there is no way this doesnt work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/DriveSubsystemNikolas.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/DriveSubsystemNikolas.java
@@ -41,6 +41,25 @@ public class DriveSubsystemNikolas {
         //double max = Math.abs(forward) + Math.abs(strafe);
         
     }
+    public void driveFieldCentric(double forward, double strafe, double turn, double angle) {
+        double angleTo = Math.atan(strafe/forward);
+        double angleOffset = angleTo - angle;
+
+        forward = Math.cos(angleOffset);
+        strafe = Math.sin(angleOffset);
+
+        double flP = clamp(forward, strafe, turn);
+        double frP = clamp(forward, -strafe, -turn);
+        double blP = clamp(forward, -strafe, turn);
+        double brP = clamp(forward, strafe, -turn);
+
+        frontLeft.set(flP);
+        frontRight.set(frP);
+        backLeft.set(blP);
+        backRight.set(brP);
+
+    }
+
     public double clamp(double f, double s, double t) {
         return Math.max(-1, Math.min(1, f+s+t));
     }
